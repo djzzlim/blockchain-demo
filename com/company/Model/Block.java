1a4b858efdb5c3f6c3e7647f0709c870ae5bcba1
Lim Kar Joon <djzzlim@gmail.com>
2025-06-18 00:27:53 +0800

    Added Block.java

--- com/company/Model/Block.java
+++ com/company/Model/Block.java
@@ -1,11 +1,13 @@
 package com.company.Model;
 
-import sun.security.provider.DSAPublicKeyImpl;
-
 import java.io.Serializable;
 import java.security.InvalidKeyException;
+import java.security.KeyFactory;
+import java.security.PublicKey;
 import java.security.Signature;
 import java.security.SignatureException;
+import java.security.spec.X509EncodedKeySpec;
+import java.security.spec.InvalidKeySpecException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
@@ -33,6 +35,7 @@ public class Block implements Serializable {
 		this.miningPoints = miningPoints;
 		this.luck = luck;
 	}
+	
 	// This constructor is used when we initiate it after retrieve.
 	public Block(LinkedList<Block> currentBlockChain) {
 		Block lastBlock = currentBlockChain.getLast();
@@ -40,14 +43,67 @@ public class Block implements Serializable {
 		ledgerId = lastBlock.getLedgerId() + 1;
 		luck = Math.random() * 1000000;
 	}
+	
 	// This constructor is used only for creating the first block in the blockchain
 	public Block() {
 		prevHash = new byte[]{0};
 	}
 
 	public Boolean isVerified(Signature signing) throws InvalidKeyException, SignatureException {
-		signing.initVerify(new DSAPublicKeyImpl(this.minedBy));
+		X509EncodedKeySpec keySpec = new X509EncodedKeySpec(this.minedBy);
+		KeyFactory keyFactory = KeyFactory.getInstance("DSA");
+		PublicKey publicKey = keyFactory.generatePublic(keySpec);
+		
+		signing.initVerify(publicKey);
 		signing.update(this.toString().getBytes());
 		return signing.verify(this.currHash);
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (!(o instanceof Block)) return false;
+		Block block = (Block) o;
+		return Arrays.equals(getPrevHash(), block.getPrevHash());
+	}
+
+	@Override
+	public int hashCode() {
+		return Arrays.hashCode(getPrevHash());
+	}
+
+	public byte[] getPrevHash() { return prevHash; }
+	public byte[] getCurrHash() { return currHash; }
+	public void setPrevHash(byte[] prevHash) { this.prevHash = prevHash; }
+	public void setCurrHash(byte[] currHash) { this.currHash = currHash; }
+
+	public ArrayList<Transaction> getTransactionLedger() { return transactionLedger; }
+	public void setTransactionLedger(ArrayList<Transaction> transactionLedger) { this.transactionLedger = transactionLedger; }
+
+	public String getTimeStamp() { return timeStamp; }
+	public void setTimeStamp(String timeStamp) { this.timeStamp = timeStamp; }
+
+	public byte[] getMinedBy() { return minedBy; }
+	public void setMinedBy(byte[] minedBy) { this.minedBy = minedBy; }
+
+	public Integer getMiningPoints() { return miningPoints; }
+	public void setMiningPoints(Integer miningPoints) { this.miningPoints = miningPoints; }
+
+	public Double getLuck() { return luck; }
+	public void setLuck(Double luck) { this.luck = luck; }
+
+	public Integer getLedgerId() { return ledgerId; }
+	public void setLedgerId(Integer ledgerId) { this.ledgerId = ledgerId; }
+
+	@Override
+	public String toString() {
+		return "Block{" + 
+					"prevHash=" + Arrays.toString(prevHash) +
+					", timeStamp=" + timeStamp + '\'' +
+					", minedBy=" + Arrays.toString(minedBy) +
+					", ledgerId=" + ledgerId +
+					", miningPoints=" + miningPoints +
+					", luck=" + luck +
+					'}';
 	}
 }
