835fec43a2357891f78b398c403e4bba6cd82f48
Lim Kar Joon <djzzlim@gmail.com>
2025-06-20 12:25:11 +0800

    Added MiningThread.java

---
+++ com/company/Threads/MiningThread.java
@@ -0,0 +1,36 @@
+package com.company.Threads;
+
+import com.company.ServiceData.BlockchainData;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+public class MiningThread extends Thread{
+
+    @Override
+    public void run() {
+        while (true) {
+            long lastMinedBlock = LocalDateTime.parse(BlockchainData.getInstance().getCurrentBlockChain().getLast().getTimeStamp()).toEpochSecond(ZoneOffset.UTC);
+            if ((lastMinedBlock + BlockchainData.getTimeoutInterval()) < LocalDateTime.now().toEpochSecond(ZoneOffset.UTC)) {
+                System.out.println("Blockchain is too old for mining! Update it from peers");
+            } else if (((lastMinedBlock + BlockchainData.getMiningInterval()) - LocalDateTime.now().toEpochSecond(ZoneOffset.UTC)) > 0) {
+                System.out.println("Blockchain is current, mining will commence in " +
+                    ((lastMinedBlock + BlockchainData.getMiningInterval()) - LocalDateTime.now().toEpochSecond(ZoneOffset.UTC)) + " seconds"
+                );
+            } else {
+                System.out.println("MINING NEW BLOCK");
+                BlockchainData.getInstance().mineBlock();
+                System.out.println(BlockchainData.getInstance().getWalletBalanceFX());
+            }
+            System.out.println(LocalDateTime.parse(BlockchainData.getInstance().getCurrentBlockchain().getLast().getTImeStamp()).toEpochSecond(ZoneOffset.UTC));
+            try {
+                Thread.sleep(2000);
+                if (BlockchainData.getInstance().isExit()) { break; }
+                BlockchainData.getInstance().setMiningPoints(BlockchainData.getInstance().getMiningPoints() + 2);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
